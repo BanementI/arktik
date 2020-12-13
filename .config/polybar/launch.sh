72d9aa260d855f725e0bf5221ef5832e3a220527
banement <47301693+BanementI@users.noreply.github.com>
2020-12-13 16:23:29 +0000

    Add files via upload

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# polybar -rq dummy & 
+polybar -rq left &
+polybar -rq right &
+polybar -rq righter &
+
+
+echo "Polybar launched..."
